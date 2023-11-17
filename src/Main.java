53f7a124924c23274bc930523aca684418e1e0d0
Gabi P <gabrielpetruccelli@gmail.com>
2023-11-17 10:07:34 -0300

    actualizacion

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<String> nombres = Arrays.asList("Gabriel", "Pablo", "Cesar", "Elizabeth", "Alejandro", "Ariel");
+        String nombresEnMay = nombres.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.joining(", "));
+        System.out.println(nombresEnMay);
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
